55d6ca599b38a543b7162715b1b7f04e46d2280e
Antonio Lain <antlai@cafjs.com>
2018-07-27 17:13:38 -0700

    Update mkBoard tool

--- utils/mkBoard.js
+++ utils/mkBoard.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
 var parseArgs = require('minimist');
-var cv = require('./opencv.js');
 var PNG = require('pngjs').PNG;
 var fs = require('fs');
-var fec = require('./fec.js');
+
+var cv = require('./opencv');
+var cvPromise = (function() {
+    return new Promise((resolve, reject) => {
+        cv.onRuntimeInitialized = () => {
+            console.log('Init cv OK');
+            resolve('Done');
+        };
+        cv.onAbort = (err) => {
+            reject(err);
+        };
+    });
+})();
 
 
-var cleanup = fec.cwrap('cleanup', null, []);
-var encode = fec.cwrap('encode', 'number', ['number', 'number']);
+var fec = require('./fec')({});
+var fecPromise = (function() {
+    return new Promise((resolve, reject) => {
+        fec.then(() => {
+            resolve('Done');
+        });
+    });
+})();
 
 var NUM_SYMBOLS = 20;
 var NUM_DATA = 8;
@@ -18,7 +35,31 @@ var N_ROWS = 5;
 var WIDTH = 2700;
 var HEIGHT = 1800;
 
-var mkBoard = function(outFile, dataList) {
+var WIDTH_PROJECT = 1280;
+var HEIGHT_PROJECT = 720;
+
+var SQUARE_PIXELS = 134;
+var TOP_PIXELS = SQUARE_PIXELS  + 25;
+var LEFT_PIXELS = SQUARE_PIXELS  + 104;
+
+var mkBoard = async function(outFile, project, dataList) {
+    var width = WIDTH;
+    var height = HEIGHT;
+    var margin = 150;
+    if (project) {
+        width = WIDTH_PROJECT;
+        height = HEIGHT_PROJECT;
+        margin = 25;
+    };
+
+    await cvPromise;
+    await fecPromise;
+
+
+    var cleanup = fec.cwrap('cleanup', null, []);
+    var encode = fec.cwrap('encode', 'number', ['number', 'number']);
+    var decode = fec.cwrap('decode', 'number', ['number', 'number', 'number',
+                                                'number']);
 
     var data = fec._malloc(NUM_SYMBOLS);
     var erasure = fec._malloc(NUM_SYMBOLS);
@@ -28,8 +69,13 @@ var mkBoard = function(outFile, dataList) {
     for (let i = 0; i< NUM_DATA; i++) {
         dataArray[i] = (dataList ? dataList[i] : Math.floor(Math.random()*32));
     }
+    erasureArray.set([], 0);
 
     encode(data, NUM_SYMBOLS);
+
+    var id = decode(data, NUM_SYMBOLS, erasure, 0);
+
+    console.log('ID: ' + id);
 
     var str = dataArray.toString().replace(/,/g, "-");
     console.log(str);
@@ -47,15 +93,48 @@ var mkBoard = function(outFile, dataList) {
     old.delete();
 
     var dest = new cv.Mat();
-    p.draw(new cv.Size(WIDTH, HEIGHT), dest, 150, 1);
+    //    p.draw(new cv.Size(width, height), dest, 150, 1);
+    p.draw(new cv.Size(width, height), dest, margin, 1);
     var destColor = new cv.Mat();
     cv.cvtColor(dest, destColor, cv.COLOR_GRAY2RGBA);
 
-    cv.putText(destColor, str, {x: 150, y: HEIGHT - 80}, cv.FONT_HERSHEY_PLAIN,
-               3.0, new cv.Scalar(0, 0, 255, 255), 2);
 
-    var buffer = PNG.sync.write({data: destColor.data, width: WIDTH,
-                                 height: HEIGHT, colorType: 6});
+    if (project) {
+        var strArray = str.split('-');
+        for (let i = 0; i<strArray.length; i++) {
+             cv.putText(destColor, strArray[i], {x: 25, y: 75 + 30*i},
+                        cv.FONT_HERSHEY_PLAIN,
+                        2.0, new cv.Scalar(0, 0, 255, 255), 2);
+        }
+        var idArray  = '' + id;
+        for (let i = 0; i<idArray.length; i++) {
+            cv.putText(destColor, idArray[i], {x: width - 50, y: 200 + 30*i},
+                       cv.FONT_HERSHEY_PLAIN,
+                       2.0, new cv.Scalar(0, 0, 255, 255), 2);
+        }
+        var result = '[';
+        var x = LEFT_PIXELS;
+        for (let i = 0; i< N_COLUMNS-1; i++) {
+            var y = TOP_PIXELS;
+            for (let j = 0; j< N_ROWS-1; j++) {
+                result = result + '(' + x + ', ' + y + '), '; // inefficient...
+                y = y + SQUARE_PIXELS;
+            }
+            result = result + '\n';
+            x = x +  SQUARE_PIXELS;
+        }
+        console.log(result + ']');
+    } else {
+        cv.putText(destColor, str, {x: 150, y: height - 80},
+                   cv.FONT_HERSHEY_PLAIN,
+                   3.0, new cv.Scalar(0, 0, 255, 255), 2);
+
+        cv.putText(destColor, '' + id, {x: width-550, y: height - 80},
+                   cv.FONT_HERSHEY_PLAIN,
+                   3.0, new cv.Scalar(0, 0, 255, 255), 2);
+    }
+    var buffer = PNG.sync.write({data: destColor.data, width: width,
+                                 height: height, colorType: 6});
     fs.writeFileSync(outFile, buffer);
 
     fec._free(data);
@@ -79,7 +158,7 @@ var usage = function(x) {
          return true;
      } else {
          console.log('Invalid ' + x);
-         console.log('Usage:  mkBoard.js <outputFile>');
+         console.log('Usage:  mkBoard.js [--project <bool>] <outputFile>');
          process.exit(1);
          return false;
      }
@@ -90,6 +169,7 @@ var args = process.argv.slice(2);
 
 var argv = parseArgs(args, {
     string : ['outputFile'],
+    boolean: ['project'],
     unknown: usage
 });
 
@@ -99,5 +179,5 @@ condInsert(argv, 'outputFile', outF);
 if (!argv.outputFile) {
     usage('outputFile');
 } else {
-    mkBoard(argv.outputFile);
+    mkBoard(argv.outputFile, argv.project);
 }
